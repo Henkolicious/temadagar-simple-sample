38ed615c72c8459913b464262657cd93105fb3f8
Henrik Larsson_c <henrik.larsson_c@sandvik.com>
2024-04-11 21:38:01 +0200

    init 🚓

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# temadagar-simple-sample
+# Temadagar.se/kalender/
+
+.NET 8
+
+From `/src` -> `dotnet run`
+
+Super simple temadagar html parser.
+
+<i>Note</i>
+<i>Hard coded for 2024</i>
+
+## Sample usage
+
+Forward output
+`dotnet run >> output.txt`
+
+View output
+`cat .\output.txt`
---
+++ src/Program.cs
@@ -0,0 +1,65 @@
+﻿using HtmlAgilityPack;
+
+var htmlResponse = await GetHtmlDocument("https://temadagar.se/kalender/");
+var document = new HtmlDocument();
+document.LoadHtml(htmlResponse);
+
+var nodes = document.DocumentNode.SelectNodes("//a");
+var ignoreText = true;
+var isFirstTimePrintEdgeCase = true;
+
+foreach (var node in nodes)
+{
+    if (string.IsNullOrWhiteSpace(node.InnerText))
+        continue;
+
+    if (char.IsDigit(node.InnerText[0]))
+        ignoreText = false;
+
+    if (ignoreText)
+        continue;
+
+    PrettyOutput(node);
+
+    var isEndOfFile = node
+        .InnerText
+        .Equals
+        (
+            "Internationella Expect a better tomorrow-dagen", 
+            StringComparison.CurrentCultureIgnoreCase
+        );
+
+    if (isEndOfFile)
+        break;
+}
+
+static async Task<string> GetHtmlDocument(string url)
+{
+    using var client = new HttpClient();
+    var response = await client.GetAsync(url);
+    return await response.Content.ReadAsStringAsync();
+}
+
+void PrettyOutput(HtmlNode node)
+{
+    var startsWithDigit = char.IsDigit(node.InnerText[0]);
+
+    if (startsWithDigit)
+    {
+        if (isFirstTimePrintEdgeCase)
+        {
+            isFirstTimePrintEdgeCase = false;
+            Console.WriteLine(node.InnerText);
+            return;
+        }
+        else
+        {
+            Console.WriteLine();
+        }
+
+        Console.WriteLine(node.InnerText);
+        return;
+    }
+
+    Console.WriteLine($"  {node?.InnerText}");
+}
